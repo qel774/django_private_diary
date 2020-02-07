4c83dad6a27f364255170fc474e1f9b60d9629ab
qel774 <hogehoge@example.com>
2020-02-08 03:44:56 +0900

    STATICフォルダ追加　TOPページ完成

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 #ロギング
 LOGGING = {
     'version': 1,
